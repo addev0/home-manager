76b6546ed51108072b37815bd4a6787c31da4166
addev <adrian.amzca0@gmail.com>
2026-02-08 12:36:34 +0800

    ExtraNvim: Added Mini-Icons

--- extra/configs/nvim/lua/plugins/statusline.lua
+++ extra/configs/nvim/lua/plugins/statusline.lua
@@ -5,8 +5,19 @@ return {
     "nvim-mini/mini.statusline",
     version = false,
     ---@type PluginOpts
+    dependencies = {
+      {
+        "nvim-mini/mini.icons",
+        version = false,
+        config = function()
+          require("mini.icons").setup()
+          require("mini.icons").mock_nvim_web_devicons()
+        end,
+      },
+    },
     opts = {
       use_icons = true,
+      set_vim_settings = false, -- Optinoal: prevents overwriting global statusline.
     },
   },
 }
