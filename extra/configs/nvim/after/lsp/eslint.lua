297f169e0500f4c64f5cf5ff13a6fbfed4b7e7ad
addev <adrian.amzca0@gmail.com>
2026-02-11 18:31:34 +0800

    Extra.Nvim: Defined Filetypes for EsLint and TS-LS

---
+++ extra/configs/nvim/after/lsp/eslint.lua
@@ -0,0 +1,12 @@
+---@type vim.lsp.Config
+return {
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "vue",
+    "svelte",
+    "astro",
+  },
+}
