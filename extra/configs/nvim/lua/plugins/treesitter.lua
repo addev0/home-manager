c5979807e6e02eb4d37855645a788368d7c08419
addev <adrian.amzca0@gmail.com>
2026-04-21 19:22:02 +0800

    Nvim TreeSitterManager Installed

--- extra/configs/nvim/lua/plugins/treesitter.lua
+++ extra/configs/nvim/lua/plugins/treesitter.lua
@@ -1,39 +1,40 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        branch = "main",
-        lazy = false,
-        build = ":TSUpdate",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
 
-        config = function()
-            local ts = require("nvim-treesitter")
+    config = function()
+      local ts = require("nvim-treesitter")
 
-            ts.setup()
-            
-            -- Checks and installs specified language-parsers asynchronously
-            ts.install({
-                -- Essentials
-                "lua", "luadoc", "vim", "vimdoc", "query",
-                -- Shell & System
-                "bash", "diff", "regex",
-                -- C Family
-                "c", 
-                -- Lisp Family
-                "commonlisp",
-                -- Web & Data
-                "markdown", "markdown_inline", "html",
-                "json", -- "jsonc" (deprecated),
-                -- Scripting
-                "nix", "powershell",
-                "yaml",
-            })
+      ts.setup()
 
-            -- Autocmd that starts nvim-treesitter in File-Open
-            vim.api.nvim_create_autocmd("FileType", {
-                callback = function()
-                    pcall(vim.treesitter.start)
-                end
-            })
-        end,
-    },
+      -- Checks and installs specified language-parsers asynchronously
+      ts.install({
+        -- Essentials
+        "lua", "luadoc", "vim", "vimdoc", "query",
+        -- Shell & System
+        "bash", "diff", "regex",
+        -- C Family
+        "c",
+        -- Lisp Family
+        "commonlisp",
+        -- Web & Data
+        "markdown", "markdown_inline", "html",
+        "javascript", "typescript",
+        "json", -- "jsonc" (deprecated),
+        -- Scripting
+        "nix", "powershell",
+        "yaml", "python"
+      })
+
+      -- Autocmd that starts nvim-treesitter in File-Open
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end
+      })
+    end,
+  },
 }
---
+++ extra/configs/nvim/lua/plugins/tsmanager.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "romus204/tree-sitter-manager.nvim",
+    dependencies = { "tree-sitter/tree-sitter" },
+    branch = "main",
+    lazy = false,
+
+    config = function()
+      local tsm = require("tree-sitter-manager")
+
+      tsm.setup({
+        install = {
+          -- Essentials
+          "lua", "luadoc", "vim", "vimdoc", "query",
+          -- Shell & System
+          "bash", "diff", "regex",
+          -- C Family
+          "c",
+          -- Lisp Family
+          "commonlisp",
+          -- Web & Data
+          "markdown", "markdown_inline", "html",
+          "json", -- "jsonc" (deprecated),
+          -- Scripting
+          "nix", "powershell",
+          "yaml",
+        },
+      })
+
+      -- Checks and installs specified language-parsers asynchronously
+
+      -- Autocmd that starts nvim-treesitter in File-Open
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+            pcall(vim.treesitter.start)
+        end
+      })
+    end,
+  },
+}
