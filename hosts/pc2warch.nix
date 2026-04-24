0b3f86e47bc5d9a86226472767590462945ce0e4
addev <adrian.amzca0@gmail.com>
2026-04-24 11:10:49 +0800

    Ubuntu Fix: pc2warch initcontent zsh

--- hosts/pc2warch.nix
+++ hosts/pc2warch.nix
@@ -1,13 +1,28 @@
 { config, pkgs, lib, extra, ... }:
 {
-    imports = [ ../home.nix ];
-    programs.zsh.initContent = lib.mkMerge [
-        (lib.mkOrder 550''
-            # Use arrow keys to navigate completion menu.
-            zstyle ':completion:*' menu select
-        '')
-        (lib.mkOrder 1000 ''
-            source "${extra.configs}/zsh/init-sshtogh-pc2.zsh"
-        '')
-    ];
+  imports = [ ../home.nix ];
+
+  programs.zsh.initContent = lib.mkMerge [
+    # Use lib.mkOrder with a space before the string
+    (lib.mkOrder 550 ''
+      # Use arrow keys to navigate completion menu.
+      zstyle ':completion:*' menu select
+    '')
+
+    (lib.mkOrder 1000 ''
+      source "${extra.configs}/zsh/init-sshtogh-pc2.zsh"
+    '')
+
+    # This is your "Nix persistence" bridge
+    (lib.mkOrder 1100 ''
+      if [ -e /nix/var/nix/profiles/default/etc/profile.d/nix.sh ]; then
+        . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+      fi
+      
+      # Also ensure your user profile is in the PATH
+      if [ -e "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
+        . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+      fi
+    '')
+  ];
 }
